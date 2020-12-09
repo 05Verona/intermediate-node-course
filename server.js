9e88c61784e21daf0af51e9e125d20c18f80df6f
05Verona <vinicius.verona@aluno.ufop.edu.br>
2020-12-09 15:16:01 -0300

    add refactoring code

--- server.js
+++ server.js
@@ -8,9 +8,38 @@ mongoose.connect('mongodb://localhost/userData') // Connect to local Mongoose DB
 
 app.use(bodyParser.json());
 
-app.listen(port, ()=>{
+app.listen(port, () => {
 	console.log(`server is listening on port:${port}`)
 })
+
+
+/**
+  {
+    name: req.body.newData.name,
+    email: req.body.newData.email,                                    is equal to {...req.body.newData} using JS spread syntax
+    password: req.body.newData.password
+  }
+*/
+
+function sendResponse(res, err, data){
+  if (err){
+    res.json({
+      success: false,
+      message: err
+    })
+  } else if (!data){
+    res.json({
+      success: false,
+      message: "Not Found!"
+    })
+  } else{
+    res.json({
+      success: true,
+      data: data
+    })
+  }
+}
+
 
 /** 
  * CREATE;
@@ -19,107 +48,35 @@ app.listen(port, ()=>{
  * Second Parameter is the db response handler. 
 */
 app.post('/users',(req,res)=>{
-  User.create({ // Takes a newData and a callback
-    name: req.body.newData.name,
-    email: req.body.newData.email,
-    password: req.body.newData.password
-  },
-
-  (err, data) => {
-    if (err){
-      res.json({success:false, message: err})
-    } else if (!data){
-      res.json({success: false, message: 'Not Found!'})
-    } else {
-      res.json({success: true, message: data})
-    }
-  })
+  User.create(// Takes a newData and a callback
+  {...req.body.newData},
+  (err, data) => sendResponse(res, err, data))
 })
 
 app.route('/users/:id')
 // READ
-.get((req,res)=>{ // Takes an ID and callback
+.get((req,res) => { // Takes an ID and callback
   User.findById(
     req.params.id,
-    
-    (err,data) => {
-      if (err){
-        res.json({
-          success: false,
-          message: err
-        })
-      } else if (!data){
-        res.json({
-          success: false,
-          message: "Not Found!"
-        })
-      } else{
-        res.json({
-          success: true,
-          message: data
-        })
-      }
-  })
+    (err,data) => sendResponse(res, err, data))
 })
 
 // UPDATE
-.put((req,res)=>{
+.put((req,res) => {
   User.findByIdAndUpdate( // Takes an ID, newData, options (optional), callback
     req.params.id,
+    {...req.body.newData},
     
-    {
-      name: req.body.newData.name,
-      email: req.body.newData.email,
-      password: req.body.newData.password
-    },
-
     {
       new: true // Make sure the update method will return the modified document
     },
-
-    (err, data) => {
-      if (err){
-        res.json({
-          success: false,
-          message: err
-        })
-      } else if (!data){
-        res.json({
-          success: false,
-          message: "Not Found!"
-        })
-      } else{
-        res.json({
-          success: true,
-          data: data
-        })
-      }
-    }
-  )
+    
+    (err, data) => sendResponse(res, err, data))
 })
 
 // DELETE
-.delete((req,res)=>{
+.delete((req,res) => {
   User.findByIdAndDelete( // Takes an ID and a callback
     req.params.id,
-
-    (err, data) => {
-      if (err){
-        res.json({
-          success: false,
-          message: err
-        })
-      } else if (!data){
-        res.json({
-          success: false,
-          message: "Not Found!"
-        })
-      } else{
-        res.json({
-          success: true,
-          data: data
-        })
-      }
-    }
-  )
+    (err, data) => sendResponse(res, err, data))
 })
