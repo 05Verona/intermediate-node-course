e5751c6e3be75f9d57348b252375ec896110a2e4
05Verona <vinicius.verona@aluno.ufop.edu.br>
2020-12-09 14:31:35 -0300

    add CREATE POST and READ GET routes

--- server.js
+++ server.js
@@ -12,20 +12,60 @@ app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
 })
 
-// CREATE
+/** 
+ * CREATE;
+ * Create a new document in MongoDB;
+ * First parameter is the object containing the values for the new document (stored in req.body);
+ * Second Parameter is the db response handler. 
+*/
 app.post('/users',(req,res)=>{
-  // User.create()
+  User.create({
+    name: req.body.newData.name,
+    email: req.body.newData.email,
+    password: req.body.newData.password
+  },
+
+  (err, data) => {
+    if (err){
+      res.json({success:false, message: err})
+    } else if (!data){
+      res.json({success: false, message: 'Not Found!'})
+    } else {
+      res.json({success: true, message: data})
+    }
+  })
 })
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
-  // User.findById()
+  User.findById(
+    req.params.id,
+    (err,data) => {
+      if (err){
+        res.json({
+          success: false,
+          message: err
+        })
+      } else if (!data){
+        res.json({
+          success: false,
+          message: "Not Found!"
+        })
+      } else{
+        res.json({
+          success: true,
+          message: data
+        })
+      }
+  })
 })
+
 // UPDATE
 .put((req,res)=>{
   // User.findByIdAndUpdate()
 })
+
 // DELETE
 .delete((req,res)=>{
   // User.findByIdAndDelete()
